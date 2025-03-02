cd8d703f989340d5bf6e20b606eaca3a4d7ee83f
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2025-03-02 19:11:37 +0200

    No articles saved added

--- app/src/main/java/com/example/newsnow/presentation/common/ArticlesList.kt
+++ app/src/main/java/com/example/newsnow/presentation/common/ArticlesList.kt
@@ -1,14 +1,22 @@
 package com.example.newsnow.presentation.common
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
+import com.example.newsnow.R
 import com.example.newsnow.domain.model.Article
 import com.example.newsnow.presentation.Dimens.EXTRA_SMALL_PADDING
 import com.example.newsnow.presentation.Dimens.MEDIUM_PADDING1
@@ -42,14 +50,29 @@ fun ArticleList(
     articles: List<Article>,
     onClick: (Article) -> Unit
 ) {
-    LazyColumn(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(MEDIUM_PADDING1),
-        contentPadding = PaddingValues(all = EXTRA_SMALL_PADDING)
-    ) {
-        items(count = articles.size) {
-            val article = articles[it]
-            ArticleCard(article = article, onClick = { onClick(article) })
+    if (articles.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "No articles saved",
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(id = R.color.text_medium),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier,
+            verticalArrangement = Arrangement.spacedBy(MEDIUM_PADDING1),
+            contentPadding = PaddingValues(all = EXTRA_SMALL_PADDING)
+        ) {
+            items(count = articles.size) {
+                val article = articles[it]
+                ArticleCard(article = article, onClick = { onClick(article) })
+            }
         }
     }
 }
